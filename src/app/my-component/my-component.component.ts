f6d8abc5356fec84eb990be7dd65efdc23173899
Harvin Bejarano <harvinbejarano@gmail.com>
2018-11-27 16:13:03 -0500

    initial commit

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-my-component',
+	templateUrl: './my-component.component.html',
+	styleUrls: [ './my-component.component.scss' ],
+})
+export class MyComponentComponent implements OnInit {
+	name = 'Harvin BJ';
+	count = 0;
+	imgSource = 'https://www.facevertizing.com/wp-content/uploads/2016/05/Nailed-It-Baby-Meme-06.jpg';
+	disabled: false;
+
+	constructor() {}
+
+	ngOnInit() {}
+}

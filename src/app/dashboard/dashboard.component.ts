323206f2371827f3c39c8cb77e731e21bfc2b0e2
Harvin Bejarano <harvinbejarano@gmail.com>
2018-11-27 20:00:30 -0500

    dictives

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 	constructor() {}
 	url;
+	imageUrls = [
+		'https://media2.giphy.com/media/13fjcmvKo45MBO/200.webp?cid=3640f6095bfdd8cf444e364f3270ccff',
+		'https://media2.giphy.com/media/Um7TeqbaS33Ww/200w.webp?cid=3640f6095bfddfbe33334c424d2dc8c8',
+		'https://media1.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.webp?cid=3640f6095bfde03e503035382e3262b2',
+	];
 	ngOnInit() {}
 
 	zoomImage(imageUrl) {
